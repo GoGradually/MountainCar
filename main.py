566271a2e53c19f76e7b43e699ac2db409af988d
GoGradually <chkun3109@gmail.com>
2025-09-06 01:12:29 +0900

    Refactor DQN training loop to support multiple trials and calculate average rewards

--- main.py
+++ main.py
@@ -1,33 +1,46 @@
 import gymnasium as gym
 import matplotlib.pyplot as plt
 from agent import DQNAgent
+import time
 
-episodes = 1000
+start = time.time()
+
+episodes = 500
 sync_interval = 20
 env = gym.make("MountainCar-v0", render_mode="rgb_array")
-agent = DQNAgent()
-reward_history = []
+reward_histories = []
+for trial in range(20):
+    agent = DQNAgent()
+    reward_history = []
+    for episode in range(episodes):
+        state = env.reset()[0]
+        done = False
+        total_reward = 0
 
-for episode in range(episodes):
-    state = env.reset()[0]
-    done = False
-    total_reward = 0
+        while not done:
+            action = agent.get_action(state)
+            next_state, reward, terminated, truncated, _ = env.step(action)
+            origin_reward = reward
+            reward += abs((next_state[0] + 0.5) * next_state[1])* 10
+            done = terminated | truncated
 
-    while not done:
-        action = agent.get_action(state)
-        next_state, reward, terminated, truncated, _ = env.step(action)
-        origin_reward = reward
-        reward += abs((next_state[0] + 0.5) * next_state[1])* 10
-        done = terminated | truncated
+            agent.update(state, action, reward, next_state, done)
+            state = next_state
+            total_reward += origin_reward
+        reward_history.append(total_reward)
+        if episode % sync_interval == 0:
+            agent.sync_qnet()
+    reward_histories.append(reward_history)
+    print(trial)
 
-        agent.update(state, action, reward, next_state, done)
-        state = next_state
-        total_reward += origin_reward
-    reward_history.append(total_reward)
-    if episode % sync_interval == 0:
-        agent.sync_qnet()
 
-plt.plot(reward_history)
+end = time.time()
+
+print(f"실행 시간: {end - start:.4f}초")
+
+reward_means = [sum(col)/len(col) for col in zip(*reward_histories)]
+
+plt.plot(reward_means)
 plt.xlabel('Episodes')
 plt.ylabel('Total Reward')
 plt.show()
